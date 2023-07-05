6ef63ef258335ae9bc22f089b8ca88582191bd54
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-07-05 18:32:42 +0530

    Linking pages

--- next-course/src/pages/news/index.js
+++ next-course/src/pages/news/index.js
@@ -1,6 +1,22 @@
+import Link from "next/link";
+
 const NewsPage = () => {
   return (
-    <div>news page</div>
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/newxt-js-is-a-great-platofrm">
+            Next JS is a great platofrm
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/economy-news">
+            Ecomony
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
