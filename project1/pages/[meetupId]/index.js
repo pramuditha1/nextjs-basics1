2f095eb5e549d52839aa2a288f54906694dc0ffc
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-07-06 12:41:37 +0530

    Add index page serverside rendering props and apply nextjs concepts in meetuId page

--- project1/pages/[meetupId]/index.js
+++ project1/pages/[meetupId]/index.js
@@ -1,16 +1,50 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      id="m1"
-      title="First meetup"
-      image="https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=1024x1024&w=is&k=20&c=ATqBHzO_cgsU8v6plHxVHTgE8zgK0BfEGzixY6JYg-c="
-      address="first meetup address"
-      description="First meetup description"
+      id={props.meetupData.id}
+      title={props.meetupData.title}
+      image={props.meetupData.image}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log("meetupId: " + meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: "m1",
+        title: "First meetup",
+        image:
+          "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=1024x1024&w=is&k=20&c=ATqBHzO_cgsU8v6plHxVHTgE8zgK0BfEGzixY6JYg-c=",
+        address: "first meetup address",
+        description: "First meetup description",
+      },
+    },
+  };
+}
 export default MeetupDetails;
--- project1/pages/index.js
+++ project1/pages/index.js
@@ -9,19 +9,40 @@ const DUMMY_MEETUS = [
         description: "First meetup description",
     },
     {
-        id: "m1",
+        id: "m2",
         title: "First meetup",
         image: "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=1024x1024&w=is&k=20&c=ATqBHzO_cgsU8v6plHxVHTgE8zgK0BfEGzixY6JYg-c=",
         address: "first meetup address",
         description: "First meetup description",
     }
 ]
-const HomePage = () => {
-  return (
-    // <Layout>
-        <MeetupList meetups={DUMMY_MEETUS}/>
-    // </Layout>  
-  )
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups}/>
+  
 }
+
+export async function getServerSideProps(context){
+    const req = context.req;
+    const res = context.res;
+
+    //fetch data from an API
+    return {
+        props: {
+            meetups: DUMMY_MEETUS,
+        }
+    }
+}
+
+//SSG - Static Site Geeration
+//validate will refresh each 10 second for new changes in meetups list
+// export async function getStaticProps() {
+//     //fetch data from an API
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUS,
+//         },
+//         validate: 1 
+//     }
+// }
 
 export default HomePage
